27a1722cf0f3bead68759092ce2672edcbf9e49d
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-24 12:44:48 +0530

    [Prasanna] Add: Implement the Multiple Generic Solution

--- GenericsProblem/Program.cs
+++ GenericsProblem/Program.cs
@@ -8,8 +8,10 @@ namespace GenericsProblem
             Console.WriteLine("Welcome to the Generics Problem");
             Operation<int> operation = new Operation<int>();
             operation.FindMax(7,8,3);
-          //  operation.FindMax<float>(1.8f, 1.6f, 1.7f);
-          //  operation.FindMax<string>("a", "b", "c");
+            Operation<float> operation1 = new Operation<float>();
+            operation1.FindMax(1.8f, 1.6f, 1.9f);
+            Operation<string> operation2 = new Operation<string>();
+            operation2.FindMax("S", "b", "c");
 
         }
     }
